e94a6c1b216cf2111f216a4c4c36d6e2704eec21
Sergey <sergey.morenets@yandex.ua>
2019-06-15 15:08:11 +0300

    Task 6. Spring YAML configuration for server list

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,33 @@
-plugins { 
-  java
-  eclipse
-  id("org.springframework.boot") version "2.1.5.RELEASE" apply false
+plugins {
+    java
+    eclipse
+    id("org.springframework.boot") version "2.1.5.RELEASE" apply false
 }
 
 allprojects {
-   group = "it.discovery"
+    group = "it.discovery"
 }
 
-subprojects {  
-   apply(plugin = "java")
-   //apply(plugin = "org.springframework.boot")
+subprojects {
+    apply(plugin = "java")
+    //apply(plugin = "org.springframework.boot")
 
-   java.sourceCompatibility = JavaVersion.VERSION_11
-   java.targetCompatibility = JavaVersion.VERSION_11
+    java.sourceCompatibility = JavaVersion.VERSION_11
+    java.targetCompatibility = JavaVersion.VERSION_11
 
-   repositories {
-     jcenter()
-   }
+    repositories {
+        jcenter()
+    }
 
-   var springBootVersion = "2.1.5.RELEASE"
-   
-   dependencies {
+    var springBootVersion = "2.1.5.RELEASE"
+
+    dependencies {
         implementation(platform("org.springframework.boot:spring-boot-dependencies:$springBootVersion"))
 
-        compile("org.springframework.boot:spring-boot-devtools")        
+        compile("org.springframework.boot:spring-boot-devtools")
         compile("org.springframework.boot:spring-boot-starter-web")
         compile("org.apache.commons:commons-lang3")
-	compile("org.hibernate:hibernate-entitymanager")
+        compile("org.hibernate:hibernate-entitymanager")
 
         runtime("javax.xml.bind:jaxb-api:2.3.0")
         runtime("javax.annotation:javax.annotation-api:1.3.1")
@@ -39,7 +39,7 @@ subprojects {
         testRuntime("org.junit.jupiter:junit-jupiter-engine")
         testCompile("org.springframework.boot:spring-boot-starter-test")
 
-   } 
+    }
 }
 
 tasks.test {
