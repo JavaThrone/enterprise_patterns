59b97a836faa704ef799c90baac67d00bd35ce23
Sergey <sergey.morenets@yandex.ua>
2019-06-15 15:38:15 +0300

    Task 7. Added validation annotations

--- client-balancer/src/main/java/it/discovery/balancer/server/LoadBalancerConfiguration.java
+++ client-balancer/src/main/java/it/discovery/balancer/server/LoadBalancerConfiguration.java
@@ -3,10 +3,13 @@ package it.discovery.balancer.server;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter @Setter
 public class LoadBalancerConfiguration {
 
+    @NotEmpty
     private List<ServerDefinition> servers;
 }
--- client-balancer/src/main/java/it/discovery/balancer/server/ServerDefinition.java
+++ client-balancer/src/main/java/it/discovery/balancer/server/ServerDefinition.java
@@ -3,12 +3,21 @@ package it.discovery.balancer.server;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter @Setter
 public class ServerDefinition {
+    @NotNull
     private String id;
 
+    @NotNull
+    @Size(min = 6)
     private String url;
 
+    @NotNull
+    @Min(3)
     private String zone;
 
     private boolean enabled = true;
