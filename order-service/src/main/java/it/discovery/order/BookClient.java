786bc2adab386f9a5a3a12ae5eb7ec277ee10e05
Sergey <sergey.morenets@yandex.ua>
2019-06-15 11:47:21 +0300

    Task 3. Refactor to reuse RestTemplate in BookClient

--- order-service/src/main/java/it/discovery/order/BookClient.java
+++ order-service/src/main/java/it/discovery/order/BookClient.java
@@ -1,22 +1,16 @@
 package it.discovery.order;
 
 import it.discovery.order.dto.BookDTO;
-import org.springframework.core.env.Environment;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+@RequiredArgsConstructor
 public class BookClient {
-
-    private final String baseUrl;
 
     private final RestTemplate restTemplate;
 
-    public BookClient(Environment env) {
-        baseUrl = env.getProperty("book.service.url", "http://localhost:8080/book");
-        restTemplate = new RestTemplate();
-    }
-
     public ResponseEntity<BookDTO> findById(int id) {
-        return restTemplate.getForEntity(baseUrl + "/" + id, BookDTO.class);
+        return restTemplate.getForEntity("/" + id, BookDTO.class);
     }
 }
--- order-service/src/main/java/it/discovery/order/OrderApplication.java
+++ order-service/src/main/java/it/discovery/order/OrderApplication.java
@@ -2,9 +2,11 @@ package it.discovery.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableJpaRepositories("it.discovery.order.repository")
@@ -14,7 +16,20 @@ public class OrderApplication {
     }
 
     @Bean
-    public BookClient bookClient(Environment env) {
-        return new BookClient(env);
+    public BookClient bookClient(RestTemplate restTemplate) {
+        return new BookClient(restTemplate);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder
+                                     restTemplateBuilder,
+                                     Environment env) {
+        String baseUrl = env
+                .getProperty("book.service.url",
+                        "http://localhost:8080/book");
+
+        return restTemplateBuilder
+                .rootUri(baseUrl)
+                .build();
     }
 }
