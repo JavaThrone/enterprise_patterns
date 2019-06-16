177fc98db84b72bd52ce96a10c9acbd370507a67
Sergey <sergey.morenets@yandex.ua>
2019-06-16 11:11:07 +0300

    Task 9. Add retryable RestTemplate as a bean

--- order-service/src/main/java/it/discovery/order/OrderApplication.java
+++ order-service/src/main/java/it/discovery/order/OrderApplication.java
@@ -1,11 +1,15 @@
 package it.discovery.order;
 
+import it.discovery.balancer.RetryConfiguration;
 import it.discovery.balancer.api.LoadBalancer;
+import it.discovery.balancer.client.RetryableRestTemplate;
 import it.discovery.order.client.BookClient;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
@@ -25,9 +29,15 @@ public class OrderApplication {
     }
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder
-                                             restTemplateBuilder) {
-        return restTemplateBuilder
-                .build();
+    @Primary
+    @Qualifier("retry")
+    public RestTemplate restTemplate(RetryConfiguration retryConfiguration) {
+        return new RetryableRestTemplate(retryConfiguration);
+    }
+
+    @Bean
+    @Qualifier("no-retry")
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
