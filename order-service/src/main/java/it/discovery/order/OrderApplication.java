131cc6616af721f930f31417695eb0c0cc339093
Sergey <sergey.morenets@yandex.ua>
2019-06-16 13:55:53 +0300

    Task 11. Add conditional beans for load balancer

--- order-service/src/main/java/it/discovery/order/OrderApplication.java
+++ order-service/src/main/java/it/discovery/order/OrderApplication.java
@@ -1,13 +1,19 @@
 package it.discovery.order;
 
+import it.discovery.balancer.api.HealthCheckService;
 import it.discovery.balancer.api.LoadBalancer;
+import it.discovery.balancer.api.impl.ActuatorHealthCheckService;
+import it.discovery.balancer.api.impl.CPUUtilizationLoadBalancer;
+import it.discovery.balancer.api.impl.RandomLoadBalancer;
 import it.discovery.balancer.client.RetryableRestTemplate;
 import it.discovery.balancer.config.CircuitBreakerConfig;
 import it.discovery.balancer.config.RetryConfiguration;
+import it.discovery.balancer.server.LoadBalancerConfiguration;
 import it.discovery.order.client.BookClient;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -17,6 +23,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableJpaRepositories("it.discovery.order.repository")
 @EnableScheduling
+//@Import(LoadBalancerAutoConfiguration.class)
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
@@ -40,5 +47,23 @@ public class OrderApplication {
     @Qualifier("no-retry")
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public HealthCheckService healthCheckService(RestTemplate restTemplate,
+                                                 LoadBalancerConfiguration loadBalancerConfiguration) {
+        return new ActuatorHealthCheckService(restTemplate, loadBalancerConfiguration);
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "load.balancer", name = "type", havingValue = "metrics")
+    public LoadBalancer metricsLoadBalancer(HealthCheckService healthCheckService) {
+        return new CPUUtilizationLoadBalancer(healthCheckService);
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "load.balancer", name = "type", havingValue = "random")
+    public LoadBalancer randomLoadBalancer(HealthCheckService healthCheckService) {
+        return new RandomLoadBalancer(healthCheckService);
     }
 }
--- spring-boot-starter-load-balancer/src/main/java/it/discovery/balancer/configuration/LoadBalancerAutoConfiguration.java
+++ spring-boot-starter-load-balancer/src/main/java/it/discovery/balancer/configuration/LoadBalancerAutoConfiguration.java
@@ -1,35 +1,8 @@
 package it.discovery.balancer.configuration;
 
-import it.discovery.balancer.api.HealthCheckService;
-import it.discovery.balancer.api.LoadBalancer;
-import it.discovery.balancer.api.impl.ActuatorHealthCheckService;
-import it.discovery.balancer.api.impl.CPUUtilizationLoadBalancer;
-import it.discovery.balancer.api.impl.RandomLoadBalancer;
-import it.discovery.balancer.server.LoadBalancerConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class LoadBalancerAutoConfiguration {
-    @Bean
-    public HealthCheckService healthCheckService(RestTemplate restTemplate,
-                                                 LoadBalancerConfiguration loadBalancerConfiguration) {
-        return new ActuatorHealthCheckService(restTemplate, loadBalancerConfiguration);
-    }
 
-    @Bean
-    @ConditionalOnMissingBean(LoadBalancer.class)
-    @ConditionalOnProperty(name = "load.balancer.type", value = "random")
-    public LoadBalancer loadBalancer(HealthCheckService healthCheckService) {
-        return new RandomLoadBalancer(healthCheckService);
-    }
-
-    @Bean
-    @ConditionalOnProperty(name = "load.balancer.type", value = "metrics")
-    public LoadBalancer metricsLoadBalancer(HealthCheckService healthCheckService) {
-        return new CPUUtilizationLoadBalancer(healthCheckService);
-    }
 }
